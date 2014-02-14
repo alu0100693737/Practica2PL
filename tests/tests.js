f5a35151d62a1ae68b7e8efbe6d45ea95e1f4a40
GARCIA CAMPOS <alu4394@cc376.(none)>
2014-02-14 09:21:40 +0000

    Arreglados los tests

--- tests/tests.js
+++ tests/tests.js
@@ -4,8 +4,8 @@ suite('CSV', function() {
       test('Prueba con CSV incorrecto', function() {
 	  original.value = '"producto", "precio" \n "libro de O\'Reilly", "7,2"';
 	  calculate();
-	  assert.equal(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr> <td>producto</td> <td>precio</td> </tr>\n<tr> <td>libro de O\'Reilly</td> <td>7,2</td> </tr>\n</tbody></table>');
-	  assert.notMatch(finaltable.innerHTML, /class="error"/);
+	  assert.equal(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr> <td>producto</td>  <td>precio</td> </tr>\n<tr> <td>libro de O\'Reilly</td>  <td>7,2</td> </tr>\n</tbody></table>');
+	  
       });
 
       test('Prueba con CSV correcto', function() {
